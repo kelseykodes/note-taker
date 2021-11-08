5170a4e9fb9cdc1894518274a01714337060a590
kperkins97 <perkins.kelsey@yahoo.com>
2021-11-08 15:49:34 -0600

    creating POST route

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 const router = express.Router();
+// const uuid = require('./helpers/uuid');
 const PORT = process.env.PORT || 5500;
 
 // middleware for JSON parsing and urlencoded form data
@@ -13,16 +15,53 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
+//GET route for the note taking page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+// Helper function
+const uuid = () => {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+};
+
+app.post('/notes', (req, res) => {
+  // Informs client that a POST request was received
+  res.json(`${req.method} request received to add your new note`);
+
+  //Informs developer that a POST request was received in terminal
+  console.info(`${req.method} request received to add user note`);
+
+  // Destructuring assignment for the items in req.body
+  const addNote = req.body;
+
+  // if (addNote) {
+
+  // }
+  //giving new not an id
+  addNote.id = uuid()
+
+  // Read notes 
+  const data = JSON.parse(fs.readFileSync("./db/db.json"));
+
+  // Push new note to file
+  data.push(addNote);
+
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(data))
+
+
+  console.log("note created");
+  res.json(data);
+
+});
 
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} !`)
 );
-
 
 
 
